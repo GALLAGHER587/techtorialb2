72db91cd0d55f37a2ca2928f6d58602a310e0880
GALLAGHER587 <ogallagher587@gmail.com>
2023-01-07 11:15:18 -0600

    SimpleMath.py and hello.py is commited

---
+++ SimpleMath.py
@@ -0,0 +1,12 @@
+# In the lines below we are going to print number and the 
+#text in the same line seperated by comma 
+
+print("8 times 7 is", 7 * 8)
+
+print("8 minus 7 is", 8 - 7)
+
+print ("8 plus 7 is", 8+7)
+
+print("8 divided by 7 is", 8/7)
+
+#Everything in single or double qoutation is called string in python 
---
+++ hello.py
@@ -0,0 +1,37 @@
+
+# Note: Line below printed the text with double quotation 
+print("Hello World")
+
+print('Hello World')
+
+# Hightlight the parts you want to comment and 
+# press command + / 
+
+# What if I want to print multiple lines with 
+# one print function?
+# We have to use triple double quotation or
+# triple single quotation
+# Example 
+print("""Hello ,
+This is an example for 
+multiline in single print function""")
+
+print ('''Hello,
+This is the exammple for multiline 
+in single print function using triple single quotation''')
+
+
+# I'd like to watch a movie 
+
+print ("I'd like to watch a movie")
+
+# I'm fond of the saying "something"
+
+# When we define the senstence above in a python text 
+# we should let python know which quotations it should not consider 
+# as a python code 
+# How to tell python it is in the text is using \ (backslash)
+print ("I'm fond if the saying \"somthing\"")
+
+#How do I define the sentence in a single quotated text?
+print ('I\'m found of the saying "somthing"')
